ae72ef468d0e0d4eb0711c4798e0066f7710666e
diwash nembang <diwashnembang0@gmail.com>
2024-04-21 00:02:45 +1000

    added resume icon

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,25 @@
 import { Grid, Box, GridItem, Image, Flex, Center } from "@chakra-ui/react";
 import logo from "../assets/image/Logo.svg";
+import ResumeButton from "./utils/ResumeButton";
 import React from "react";
 
 const Nav = () => {
   return (
-    <Center w="100vw">
+    <Center w="100vw" _hover={{ cursor: "pointer" }}>
       <Flex
+        maxW="1280px"
         w="1280px"
         mx="40px"
+        my="15px"
         justifyContent="space-between"
         // border="solid green 3px"
         alignItems="center"
         h="56px"
       >
-        <Box id="personal" className="text">
+        <Box>
           <Image src={logo} w="139px" h="40px" />
         </Box>
-        <Box id="info">
+        <Box>
           <Grid templateColumns="repeat(4,1fr)">
             <GridItem className="text" mx="5px">
               About me
@@ -32,8 +35,11 @@ const Nav = () => {
             </GridItem>
           </Grid>
         </Box>
-        <Box id="resume" className="text">
-          resume
+        <Box
+          _hover={{ "& svg rect": { fill: "#404040" } }}
+          _active={{ "& svg .test": { fill: "#FFFFFF" } }}
+        >
+          <ResumeButton width={158} height={56} color={"black"} />
         </Box>
       </Flex>
     </Center>
