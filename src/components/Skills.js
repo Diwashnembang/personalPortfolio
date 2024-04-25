5890bf3fdd7f25e257f56dd995ef2195c75b9958
DiwashKOI <167664783+DiwashKOI@users.noreply.github.com>
2024-04-26 00:04:06 +1000

    added sills componets with logos

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,18 @@
-import { Box } from "@chakra-ui/react";
+import { Box, SimpleGrid, Image } from "@chakra-ui/react";
 import React from "react";
-
+import { logos } from "./utils/SkillsLogo";
 const Skills = () => {
-  return <Box id="Skills">this is skills</Box>;
+  return (
+    <Box id="Skills">
+      <SimpleGrid columns={[2, 4, 6]}>
+        {Object.keys(logos).map((logo, index) => (
+          <Box key={index}>
+            <Image src={logos[logo]}></Image>
+          </Box>
+        ))}
+      </SimpleGrid>
+    </Box>
+  );
 };
 
 export default Skills;
---
+++ src/components/utils/SkillsLogo.js
@@ -0,0 +1,12 @@
+import react from "../../assets/image/icon-react.png";
+import git from "../../assets/image/icon-git.png";
+import express from "../../assets/image/icon-express.svg";
+import js from "../../assets/image/icon-javscript.svg";
+import mongoDb from "../../assets/image/icon-mongodb.svg";
+export const logos = {
+  react,
+  git,
+  express,
+  js,
+  mongoDb,
+};
