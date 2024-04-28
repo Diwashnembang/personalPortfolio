f4bc82fe48257ca7ceed4273fd9da4fbcff967c6
DiwashKOI <167664783+DiwashKOI@users.noreply.github.com>
2024-04-29 00:26:28 +1000

    styled skill component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,17 +1,36 @@
-import { Box, SimpleGrid, Image } from "@chakra-ui/react";
+import {
+  Center,
+  SimpleGrid,
+  Image,
+  Card,
+  CardBody,
+  VStack,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
 import { logos } from "./utils/SkillsLogo";
 const Skills = () => {
+  console.log(logos);
   return (
-    <Box id="Skills">
-      <SimpleGrid columns={[2, 4, 6]}>
+    <Center id="Skills" w={"100vw"}>
+      <SimpleGrid
+        columns={[2, 4, 6]}
+        maxW={"1280px"}
+        w={"1280px"}
+        spacing={"1.5rem"}
+      >
         {Object.keys(logos).map((logo, index) => (
-          <Box key={index}>
-            <Image src={logos[logo]}></Image>
-          </Box>
+          <Card w={"100%"} h={"150px"} mb={"1.8rem"} maxW="sm">
+            <CardBody key={index}>
+              <VStack justify={"center"} align={"center"} h={"100%"}>
+                <Image src={logos[logo].img}></Image>
+                <Text>{logos[logo].text}</Text>
+              </VStack>
+            </CardBody>
+          </Card>
         ))}
       </SimpleGrid>
-    </Box>
+    </Center>
   );
 };
 
--- src/components/utils/SkillsLogo.js
+++ src/components/utils/SkillsLogo.js
@@ -3,10 +3,29 @@ import git from "../../assets/image/icon-git.png";
 import express from "../../assets/image/icon-express.svg";
 import js from "../../assets/image/icon-javscript.svg";
 import mongoDb from "../../assets/image/icon-mongodb.svg";
-export const logos = {
-  react,
-  git,
-  express,
-  js,
-  mongoDb,
+
+const capatilizeFirstChar = (text) =>
+  text.split("").map((char, index) => {
+    let result = "";
+    if (index === 0) {
+      result = result + char.toUpperCase();
+    } else {
+      result = result + char;
+    }
+
+    return result;
+  })     ;
+const convertToOb = (skills) => {
+  const obj = {};
+  Object.keys(skills).forEach((skill) => {
+    obj[skill] = {};
+    obj[skill]["img"] = skills[skill];
+    obj[skill]["text"] = capatilizeFirstChar(skill);
+  });
+  return obj;
 };
+
+const images = { react, git, express, js, mongoDb };
+const result = convertToOb(images);
+
+export const logos = result;
