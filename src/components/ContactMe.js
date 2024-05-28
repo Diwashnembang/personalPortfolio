680d478565da7054958ba65b82ddb8e43d16a243
Diwash Nembang <nembangd0@gmail.com>
2024-05-28 22:40:58 +1000

    added form validation css

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -7,46 +7,80 @@ import {
   FormErrorMessage,
   Button,
 } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 const ContactMe = () => {
   const [isError, setIsError] = useState(false);
-  const [input, setInput] = useState("");
+  const [invalidElement, setInvalidElement] = useState(null);
+  const currentElement = useRef();
 
   const checkError = (e) => {
-    console.log("this is e.onivalid", e.target.isinvalid);
-    const check = e.target.value === "";
-    setIsError(check);
-
-    console.log(e.target.value);
+    // e.target.value === ""
+    //   ? (e.target.parentElement.isInvalid = true)
+    //   : (e.target.parentElement.isInvalid = false);
+    setIsError(e.target.value === "");
+    currentElement.current = e.target;
   };
 
   useEffect(() => {
-    console.log(isError);
+    isError
+      ? setInvalidElement(currentElement.current)
+      : setInvalidElement(null);
   }, [isError]);
 
+  const assignInvalid = (id) => {
+    invalidElement && console.log(invalidElement);
+    return isError && invalidElement && invalidElement.id === id;
+  };
+
   return (
+    //TODO :  add isInvalid to other child, sytle it and make form working
     <Center>
       <form action="/">
-        <FormControl isRequired isInvalid={isError}>
+        <FormControl isRequired isInvalid={assignInvalid("first-name")}>
           <FormLabel> first Name</FormLabel>
-          <Input placeholder="First name" onChange={checkError}></Input>
-          {isError && <FormErrorMessage>This is required</FormErrorMessage>}
+          <Input
+            placeholder="First name"
+            onChange={checkError}
+            id="first-name"
+          ></Input>
+          {isError && invalidElement && invalidElement.id === "first-name" && (
+            <FormErrorMessage>This is required</FormErrorMessage>
+          )}
         </FormControl>
-        <FormControl isRequired>
+        <FormControl isRequired isInvalid={assignInvalid("last-name")}>
           <FormLabel> last Name</FormLabel>
-          <Input placeholder="Last name"></Input>
-          {isError && <FormErrorMessage>This is required</FormErrorMessage>}
+          <Input
+            placeholder="Last name"
+            onChange={checkError}
+            id="last-name"
+          ></Input>
+          {isError && invalidElement && invalidElement.id === "last-name" && (
+            <FormErrorMessage>This is required</FormErrorMessage>
+          )}
         </FormControl>
-        <FormControl isRequired>
+        <FormControl isRequired isInvalid={assignInvalid("email")}>
           <FormLabel>email</FormLabel>
-          <Input placeholder="email" type="email"></Input>
-          {isError && <FormErrorMessage>This is required</FormErrorMessage>}
+          <Input
+            placeholder="email"
+            type="email"
+            id="email"
+            onChange={checkError}
+          ></Input>
+          {isError && invalidElement && invalidElement.id === "email" && (
+            <FormErrorMessage>This is required</FormErrorMessage>
+          )}
         </FormControl>
-        <FormControl isRequired>
+        <FormControl isRequired isInvalid={assignInvalid("message")}>
           <FormLabel>message</FormLabel>
-          <Textarea placeholder="write your message" />
-          {isError && <FormErrorMessage>This is required</FormErrorMessage>}
+          <Textarea
+            placeholder="write your message"
+            id="message"
+            onChange={checkError}
+          />
+          {isError && invalidElement && invalidElement.id === "message" && (
+            <FormErrorMessage>This is required</FormErrorMessage>
+          )}
         </FormControl>
         <Button type="submit" onClick={checkError}>
           Send
